cdc70f6fe4b6be0787585c87a2177549db2bd24a
Unknown <hc.ngu01@gmail.com>
2019-11-18 12:42:23 +0800

    added puppeteer to workspace for pdf gen, up.sh accept ssh user param

--- up.sh
+++ up.sh
@@ -4,4 +4,13 @@
 # defining all the services here
 services=('nginx' 'mysql' )
 
-docker-compose up -d "${services[@]}" && docker-compose exec workspace bash
+# argument 1 accept a user, which will be the ssh user
+# eg. ./up.sh root  will ssh as root
+if [ $# -gt 0 ]
+then
+  user=$1
+else
+  user=laradock
+fi
+
+docker-compose up -d "${services[@]}" && docker-compose exec --user="$user" workspace bash
